978e33f4b7f1d11fd63e6da09c32240605a06e50
Altiuss <altiuss@inbox.lv>
2023-02-02 22:29:10 +0100

    added footer and changed block4

--- GardenOnlineShop/garten-shop/src/components/Footer/index.jsx
+++ GardenOnlineShop/garten-shop/src/components/Footer/index.jsx
@@ -1,7 +1,27 @@
-import React from 'react'
+import React from "react";
+import s from "./index.module.css";
+import { SlSocialInstagram } from "react-icons/sl";
+import { SiWhatsapp } from "react-icons/si";
 
 export default function Footer() {
   return (
-    <div>Footer</div>
-  )
+    <footer className={s.footer}>
+      <div className={s.left_footer}>
+        <h3 className={s.contact}>Contact</h3>
+        <p className={s.phone}>+49 999 999 99 99</p>
+        <div>
+         <div className={s.soc}><SlSocialInstagram className={s.instagram}/>
+         <p className={s.soc_text}>instagram</p></div> 
+          <div className={s.soc}><SiWhatsapp  className={s.whatsapp}/>
+          <p className={s.soc_text}>WhatsApp</p></div>
+        </div>
+      </div>
+      <div className={s.right_footer}>
+        <h1>Address</h1>
+        <p>Linkstraße 2, 8 OG, 10785, Berlin, Deutschland</p>
+        <p>Working Hours:</p>
+        <p>24 hours a day</p>
+      </div>
+    </footer>
+  );
 }
--- GardenOnlineShop/garten-shop/src/pages/MainPage/main_page_components/Block4/index.jsx
+++ GardenOnlineShop/garten-shop/src/pages/MainPage/main_page_components/Block4/index.jsx
@@ -3,6 +3,10 @@ import s from "./index.module.css";
 
 export default function Block4() {
   return <section className={s.section_for}>
-    <div className={s.container_section_for}></div>
+    <div className={s.container_section_for}>
+      <div className={s.section_for__title}>
+      <h1>Sale</h1> </div>
+      <div className={s.section_for__content}></div>
+    </div>
   </section>;
 }
